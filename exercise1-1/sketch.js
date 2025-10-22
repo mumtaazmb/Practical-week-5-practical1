a1dfe986edea1bd518992fdb9eec521137a126f1
mumtaazmb <m.mahir.bare@gmail.com>
2025-10-22 12:33:31 +0100

    DO ASAP

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,9 +1,23 @@
+let circledia = [360,280,200,150];
+let colourVals = [60,140,180,255]
+
 function setup(){
     createCanvas(400,400);
 }
 
 function draw(){
-    background(255);;
-    let circledia = [360,280,200,150];
+    background(255);
+    for(let i = 0 ; i < circledia.length; i++){
+    fill(60,97,colourVals[i])    
+    circle(200,200,circledia[i])   
+
+}
+    
+
    
+}
+
+function mousePressed(){
+    colourVals.unshift (colourVals.pop())
+    
 }
--- exercise1-2/sketch.js
+++ exercise1-2/sketch.js
@@ -1,16 +1,26 @@
+starX = []
+starY = []
 function setup() {
-
+    createCanvas(400,400)
 }
 
 function draw() {
+    background(0,0,100)
+    for(let i = 0;i < starX.length ; i++){
+    star(starX[i],starY[i])
+    
 
 }
+fall()
+}
+
 
 /**
  * Draws a star at the given coordinates.
  * @param {number} x 
  * @param {number} y 
  */
+
 function star(x, y) {
     fill(255, 234, 0);
     noStroke();
@@ -19,4 +29,14 @@ function star(x, y) {
     triangle(x + 50, y - 20, x, y - 20, x, y + 10);
     triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30);
     triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30);
+}
+function mouseClicked(){
+    starX.push(mouseX)
+    starY.push(mouseY)
+}
+
+function fall(){
+    for(i = 0; i < starX.length; i++){
+        starY[i] += 2
+    }
 }
